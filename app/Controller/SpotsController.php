e192a094d53d8f268a84c05db1918e2996a99e34
justclara <justclara@me.com>
2013-04-13 23:48:42 +0200

    add description
    add description

--- app/Controller/SpotsController.php
+++ app/Controller/SpotsController.php
@@ -5,4 +5,29 @@ class SpotsController extends AppController{
 	{
 		$this->set('spots', $this->Spot->find('all'));
 	}
+
+	public function add()
+	{
+		$this->set('spots', $this->Spot->find('all'));
+	}
+
+	public function edit()
+	{
+		$this->set('spots', $this->Spot->find('all'));
+	}
+
+	public function view()
+	{
+		$this->set('spots', $this->Spot->find('all'));
+	}
+
+	public function delete()
+	{
+		$this->set('spots', $this->Spot->find('all'));
+	}
+
+	public function find()
+	{
+		$this->set('spots', $this->Spot->find('all'));
+	}
 }
