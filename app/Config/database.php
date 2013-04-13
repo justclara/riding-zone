cf346655e23d85d80f7fbd1ad73663108aefb1d7
justclara <justclara@me.com>
2013-04-13 23:23:22 +0200

    start project
    start project

---
+++ app/Config/database.php
@@ -0,0 +1,24 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => ':/Applications/MAMP/tmp/mysql/mysql.sock',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'cakephp',
+		'encoding' => 'utf8'
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => ':/Applications/MAMP/tmp/mysql/mysql.sock',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'cakephp',
+		'encoding' => 'utf8'
+	);
+}
